d901e8439eabdfb08b3f903a403fe73feb7f6aad
askoulwassim <36458905+askoulwassim@users.noreply.github.com>
2018-11-23 20:20:24 -0500

    Add files via upload

---
+++ hw20/IntArray.h
@@ -0,0 +1,23 @@
+#ifndef _IntArray_H_
+#define _IntArray_H_
+#include <iostream>
+using namespace std;
+
+
+class IntArray {
+
+  int* elt;
+  int size;
+  int DEFAULT_SIZE=4;
+
+public:
+  IntArray (int);
+  IntArray ();
+  IntArray (const IntArray&);
+  ~IntArray ();
+  IntArray operator = (const IntArray&);
+  int getSize ();
+  void display ();
+  int& operator [] (int);
+};
+#endif
